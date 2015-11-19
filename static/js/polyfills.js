b6f42751ed5fb3917dd41dc0be52f4ba114b3979
Alex Recker <alex@reckerfamily.com>
2015-11-18 19:19:39 -0600

    Polyfills: fixes grunt error

--- static/js/polyfills.js
+++ static/js/polyfills.js
@@ -2,7 +2,7 @@
     String.prototype.format = function () {
         var i = 0, args = arguments;
         return this.replace(/{}/g, function () {
-            return typeof args[i] != 'undefined' ? args[i++] : '';
+            return typeof args[i] !== 'undefined' ? args[i++] : '';
         });
     };
 }());
