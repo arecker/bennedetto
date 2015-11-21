f9f8e39ca274762ed8be8c312cf540c631ee62dc
Alex Recker <alex@reckerfamily.com>
2015-11-21 16:32:25 -0600

    Transactions: adds some of the form

---
+++ static/app/directives/bd-transactions-form.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    function bdTransactionsForm(StaticService) {
+        return  {
+            restrict: 'E',
+            templateUrl: StaticService.partial('bd-transactions-form.html'),
+            controller: 'bdTransactionsFormController',
+            controllerAs: 'transFormCtrl',
+            bindToController: true,
+            scope: {
+                afterSubmit: '=?'
+            }
+        };
+    }
+
+    function bdTransactionsFormController(TransactionsResource, FormFactory) {
+        var self = FormFactory.buildFormController(this, {
+            afterSubmit: this.afterSubmit,
+            resource: TransactionsResource
+        });
+    }
+
+    angular
+        .module('bennedetto')
+        .controller('bdTransactionsFormController', ['TransactionsResource', 'FormFactory', bdTransactionsFormController])
+        .directive('bdTransactionsForm', ['StaticService', bdTransactionsForm]);
+
+}());
