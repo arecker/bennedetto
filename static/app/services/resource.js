e4abecf10ddad98f03aa78b5fab50a7872a31b75
Alex Recker <alex@reckerfamily.com>
2015-11-21 16:05:10 -0600

    Resource: refactors to add transactions

--- static/app/services/resource.js
+++ static/app/services/resource.js
@@ -1,11 +1,25 @@
 (function() {
     'use strict';
 
-    function RatesResource($resource, APP_SETTINGS) {
-        return $resource('{}{}/:id'.format(APP_SETTINGS.apiUrl, 'rates'));
+    function ResourceFactory($resource, APP_SETTINGS) {
+        return {
+            buildResource: function(endpoint) {
+                return $resource('{}{}/:id'.format(APP_SETTINGS.apiUrl, endpoint));
+            }
+        };
+    }
+
+    function RatesResource(ResourceFactory) {
+        return ResourceFactory.buildResource('rates');
+    }
+
+    function TransactionsResource(ResourceFactory) {
+        return ResourceFactory.buildResource('transactions');
     }
 
     angular
         .module('bennedetto')
-        .service('RatesResource', ['$resource', 'APP_SETTINGS', RatesResource]);
+        .factory('ResourceFactory', ['$resource', 'APP_SETTINGS', ResourceFactory])
+        .service('RatesResource', ['ResourceFactory', RatesResource])
+        .service('TransactionsResource', ['ResourceFactory', TransactionsResource]);
 }());
