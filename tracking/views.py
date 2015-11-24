a2a14f37dc206c4d5ae7a89254d3e3a522ccf2b7
Alex Recker <alex@reckerfamily.com>
2015-11-23 21:06:01 -0600

    Tracking: adds restrict_to_user
    Restricts the rates and transactions to the owning user

--- tracking/views.py
+++ tracking/views.py
@@ -7,6 +7,13 @@ from tracking.serializers import (RateCreateSerializer,
                                   TransactionCreateSerializer)
 
 
+def restrict_to_user(func):
+    def wrapper(self, *args, **kwargs):
+        qs = func(self, *args, **kwargs)
+        return qs.user(self.request.user)
+    return wrapper
+
+
 class SpecifyCreateSerializerMixin(object):
     def get_serializer_class(self, *args, **kwargs):
         if self.action == 'create':
@@ -19,8 +26,16 @@ class RateViewSet(SpecifyCreateSerializerMixin, viewsets.ModelViewSet):
     serializer_class = RateSerializer
     serializer_create_class = RateCreateSerializer
 
+    @restrict_to_user
+    def get_queryset(self, *args, **kwargs):
+        return super(RateViewSet, self).get_queryset(*args, **kwargs)
+
 
 class TransactionViewSet(SpecifyCreateSerializerMixin, viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     serializer_create_class = TransactionCreateSerializer
+
+    @restrict_to_user
+    def get_queryset(self, *args, **kwargs):
+        return super(TransactionViewSet, self).get_queryset(*args, **kwargs)
