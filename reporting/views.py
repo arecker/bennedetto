bea9ab3167785c08b6d37f35e2b1d69ae88763f5
Alex Recker <arecker@healthgrades.com>
2015-11-27 16:10:00 -0600

    Adds more powerful routing to report view

--- reporting/views.py
+++ reporting/views.py
@@ -1,7 +1,17 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import list_route
+from django.core.urlresolvers import reverse
 
 
 class ReportViewSet(ViewSet):
+    def _to_absolute_url(self, name):
+        rel = reverse('api:{}'.format(name))
+        return self.request.build_absolute_uri(rel)
+
     def list(self, *args, **kwargs):
-        return Response({'google': 'http://google.com'})
+        return Response({'summary': self._to_absolute_url('reports-summary')})
+
+    @list_route(methods=['get'], url_path='summary')
+    def summary(self, request):
+        return Response('summary report')
