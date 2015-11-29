f168c051c19e06c43508d2e6b2806bffb2a18f47
Alex Recker <alex@reckerfamily.com>
2015-11-29 13:42:08 -0600

    Bug: restricts reports to user's transactions
    Fixes #42

--- reporting/views.py
+++ reporting/views.py
@@ -6,17 +6,18 @@ from django.core.urlresolvers import reverse
 from tracking.models import Transaction
 
 
-transactions = Transaction.objects
-
-
 class ReportViewSet(ViewSet):
     def _to_absolute_url(self, name):
         rel = reverse('api:{}'.format(name))
         return self.request.build_absolute_uri(rel)
+
+    def _get_transactions(self):
+        return Transaction.objects.user(self.request.user)
 
     def list(self, *args, **kwargs):
         return Response({'summary': self._to_absolute_url('reports-summary')})
 
     @list_route(methods=['get'], url_path='summary')
     def summary(self, request):
+        transactions = self._get_transactions()
         return Response({'today': transactions.today().total()})
