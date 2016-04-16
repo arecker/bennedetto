d665de79c1053eb0ef3d40d9bd280bc3baf8b5f5
Alex Recker <alex@reckerfamily.com>
2016-04-16 15:51:21 -0500

    Moves static root to dev settings

--- bennedetto/settings/common.py
+++ bennedetto/settings/common.py
@@ -70,7 +70,6 @@ API_URL = '/api/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
--- bennedetto/settings/dev.py
+++ bennedetto/settings/dev.py
@@ -15,6 +15,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'temp', 'email')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'temp/static')
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
